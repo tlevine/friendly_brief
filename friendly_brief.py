f4d046ec5efd333e1164f730977ff757c2f3c711
Thomas Levine <_@thomaslevine.com>
2014-05-23 22:08:30 +0000

    periods

--- friendly_brief.py
+++ friendly_brief.py
@@ -4,5 +4,5 @@ from unidecode import unidecode
 def amicus(text):
     decoded = unidecode(text)
     after_curiae = re.sub(r'curiae', '\t\t\t', decoded, flags = re.IGNORECASE, count = 1).partition('\t\t\t')[2].strip(', ').replace('\n',' ')
-    newline_delimited = re.sub(r'([^.]), ',r'\1\n', after_curiae)
+    newline_delimited = re.sub(r'([^.])[,.] ',r'\1\n', after_curiae)
     return newline_delimited.split('\n')
