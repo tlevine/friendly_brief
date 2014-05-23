d288d7cd479b73c173c121e97f8d380e75c2912d
Thomas Levine <_@thomaslevine.com>
2014-05-23 22:21:36 +0000

    dates

--- friendly_brief.py
+++ friendly_brief.py
@@ -4,5 +4,6 @@ from unidecode import unidecode
 def amicus(text):
     decoded = unidecode(text)
     after_curiae = re.sub(r'curiae', '\t\t\t', decoded, flags = re.IGNORECASE, count = 1).partition('\t\t\t')[2].strip(', ').replace('\n',' ')
-    newline_delimited = re.sub(r'([^.])[,.] ',r'\1\n', after_curiae)
-    return newline_delimited.split('\n')
+    before_date = re.sub(r'(january|february|march|april|may|june|july|august|september|october|november|december).*$', '', after_curiae, flags = re.IGNORECASE)
+    newline_delimited = re.sub(r'([a-zA-Z]{2})[,.] ',r'\1\n', before_date)
+    return filter(None, newline_delimited.split('\n'))
