560c1d9899b6b4170b0be2cde754963542c9823c
Thomas Levine <_@thomaslevine.com>
2014-05-23 21:51:22 +0000

    another test case

--- test_friendly_brief.py
+++ test_friendly_brief.py
@@ -4,6 +4,15 @@ from friendly_brief import amicus
 
 testcases = [
     ("2.    Brief, BRIEF AMICUS CURIAE OF THE GEORGIA ASSOCIATION OF BLACK ELECTED OFFICIALS IN SUPPORT OF APPELLANTS, July 19, 1996", ['THE GEORGIA ASSOCIATION OF BLACK ELECTED OFFICIALS']),
+    ("2.       Brief of amici Curiae, The Sikh Coalition, American-Arab Anti-Discrimination Committee, Discrimination and National Security Initiative, Muslim Public Affairs Council. Sikh American Legal Defense and Education Fund, Sikh Council on Religion and E ducation, South Asian Americans Leading Together and United Sikhs in Support of Respondent Iqubal", [
+        "The Sikh Coalition",
+        "American-Arab Anti-Discrimination Committee",
+        "Discrimination and National Security Initiative,",
+        "Muslim Public Affairs Council",
+        "Sikh American Legal defense and Education fund",
+        "Sikh Council on Religion and Education",
+        "South Asian Americans Leading Together",
+    ]),
 ]
 
 def check_amicus(brief, expectation):
