f8144f5f1de65dbbcdfa1b37b5c9f9bf687b3cd2
Thomas Levine <_@thomaslevine.com>
2014-05-24 19:15:43 +0000

    fix setup.py for install

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
 from distutils.core import setup
 
-setup(name='commasearch',
+setup(name='friendly_brief',
       author='Thomas Levine',
       author_email='_@thomaslevine.com',
       description='Find amicus curiae in briefs.',
       url='https://github.com/tlevine/friendly_brief',
-      packages = 'friendly_brief',
-      scripts = os.path.join('bin', 'friendly-brief'),
+      packages = ['friendly_brief'],
+      scripts = [os.path.join('bin', 'friendly-brief')],
       install_requires = ['unidecode>=0.04.16'],
       tests_require = ['nose'],
       version='0.0.1',
