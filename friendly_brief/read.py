b4b2e958cf7b48df2fb5e64850336e630d49b15c
Thomas Levine <_@thomaslevine.com>
2014-05-24 21:14:05 +0000

    better test

--- friendly_brief/read.py
+++ friendly_brief/read.py
@@ -5,6 +5,10 @@ from unidecode import unidecode
 
 def amici(brief:str) -> list:
     amici_section = re.sub(r' (:?january|february|april|may|june|july|august|september|october|november|december) [0-9]{1,2}.*', '', brief, flags = re.IGNORECASE)
+    if amici_section.lower().count(' support ') <= 1:
+        amici_section = re.sub(r' support .*', '', amici_section, flags = re.IGNORECASE)
+    if ' amici ' in amici_section[:20].lower():
+        amici_section = re.sub(r'^[^,]+amici curiae', '', amici_section, flags = re.IGNORECASE) 
     if ' and ' in amici_section.lower() and ',' in amici_section:
         buffer = 20
     elif re.search(r' inc[^a-z]', amici_section, flags = re.IGNORECASE):
